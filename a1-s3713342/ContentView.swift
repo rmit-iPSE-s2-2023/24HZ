33a3fb0df891940983f2c86990bc3921bb7fa162
minchulshin1 <s3713342@student.rmit.edu.au>
2023-08-05 02:33:58 +1000

    Initial Commit

---
+++ a1-s3713342/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  a1-s3713342
+//
+//  Created by 민철 on 2023/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ a1-s3713342/a1_s3713342App.swift
@@ -0,0 +1,17 @@
+//
+//  a1_s3713342App.swift
+//  a1-s3713342
+//
+//  Created by 민철 on 2023/08/05.
+//
+
+import SwiftUI
+
+@main
+struct a1_s3713342App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
