888c4e9c62a4ac559f23df6cc362c7c5dd41f5ca
bp094p21 <bp094p21@gmail.com>
2023-08-23 16:11:32 +1000

    init constants.swift. add: dummy TimeInterval constant

---
+++ a1-s3713342/constants.swift
@@ -0,0 +1,13 @@
+//
+//  constants.swift
+//  a1-s3713342
+//
+//  Created by fff on 2023-08-23.
+//
+
+import Foundation
+
+struct Constants {
+    // static let maxRetryAttempts = 3  // example constant
+    static let dummyCurrentTimeInterval: TimeInterval = 1688677627
+}
