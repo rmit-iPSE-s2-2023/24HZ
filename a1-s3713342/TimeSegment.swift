f53d28b2914a30552e822de125087ec3f35f6cd7
bp094p21 <bp094p21@gmail.com>
2023-08-24 23:15:38 +1000

    modify: TimeSegment to work with EventData instead of EventLog

--- a1-s3713342/TimeSegment.swift
+++ a1-s3713342/TimeSegment.swift
@@ -10,34 +10,28 @@ import SwiftUI
 struct TimeSegment: View {
     
     // Parameters
-    let eventLogs: [EventLog]?
+    var filteredEventData: [EventData]
     let toTimestamp: TimeInterval
     let fromTimestamp: TimeInterval
     let date: Date
-    let hourmarkLabel: String
     
-    init(toTimestamp: TimeInterval, eventLogs: [EventLog]? = []) {
+    init(toTimestamp: TimeInterval, eventData events: [EventData]) {
         self.toTimestamp = toTimestamp
         self.fromTimestamp = timeIntervalOfPreviousHourMark(toTimestamp: toTimestamp)
         self.date = Date(timeIntervalSince1970: toTimestamp)
-        self.hourmarkLabel = timeIntervalToHourmarkLabel(timeInterval: toTimestamp)
-        if let unwrappedEventLogs = eventLogs {
-            self.eventLogs = getEventLogsBetweenTimeIntervals(eventLogs: unwrappedEventLogs, startTime: fromTimestamp, endTime: toTimestamp)
-        } else {
-            self.eventLogs = nil
+        // Capture eventData in a local variable before using it in the closure
+        self.filteredEventData = []
+        self.filteredEventData = events.filter { event in
+            return event.eventTimestamp < toTimestamp && event.eventTimestamp >= self.fromTimestamp
         }
     }
     
     var body: some View {
 
         VStack {
-            Hourmark(label: hourmarkLabel)
-            if let unwrappedEventLogs = eventLogs {
-                ForEach(unwrappedEventLogs, id: \.id) { log in
-                    Text(log.txHash)
-                        .font(.title)
-                        .foregroundColor(Color.white)
-                }
+            Hourmark(label: timeIntervalToHourmarkLabel(timeInterval: toTimestamp))
+            ForEach(filteredEventData, id: \.eventLog.id) { event in
+                CapturedEventBlock(eventData: event)
             }
         }
         .background(Color.black)
@@ -46,6 +40,43 @@ struct TimeSegment: View {
 
 struct TimeSegment_Previews: PreviewProvider {
     static var previews: some View {
-        TimeSegment(toTimestamp: Constants.dummyCurrentTimeInterval)
+        let eventData = getEventData()
+        TimeSegment(toTimestamp: Constants.dummyCurrentTimeInterval, eventData: eventData!)
     }
 }
+
+//struct EventLogListItemView: View {
+//
+//    let isGeneric: Bool
+//    let eventLog: EventLog
+//    let eventData: EventData
+//
+//    var body: some View {
+//        HStack {
+//            VStack(alignment: .leading) {
+//                let secondaryText = isGeneric ? "Generic EventType" : "Token Name"
+//                let primaryText = isGeneric ? "Token Name" : "EventType Name"
+//                Text(secondaryText)
+//                    .font(.body)
+//                    .foregroundColor(Color.white)
+//
+//                Text(primaryText)
+//                    .font(.title.bold())
+//                Spacer()
+//            }
+//            .frame(height: 80)
+//
+//            Spacer()
+//            VStack {
+//                Text("Generic")
+//                    .foregroundColor(Color.white)
+//                Text("ERC-721")
+//            }
+//
+//        }
+//        .padding(8)
+//        .background(Color.purple)
+//        .cornerRadius(10)
+//        .padding(.leading, 40)
+//    }
+//}
