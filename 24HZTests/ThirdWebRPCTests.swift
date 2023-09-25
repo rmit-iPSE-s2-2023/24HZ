75c6a1ac2a809366aee75fad9bb0e361dc510d9f
bp094p21 <bp094p21@gmail.com>
2023-09-25 13:22:07 +1000

    add token info tests, add test for batching too many requests

--- 24HZTests/ThirdWebRPCTests.swift
+++ 24HZTests/ThirdWebRPCTests.swift
@@ -13,6 +13,10 @@ import XCTest
 final class ThirdWebRPCTests: XCTestCase {
 
     var rpc: ThirdWebRPC!
+    
+    /// Example Zora Contracts
+    let opepenThreadition = "0x6d2C45390B2A0c24d278825c5900A1B1580f9722"
+    let allure = "0x53cb0B849491590CaB2cc44AF8c20e68e21fc36D"
 
     override func setUp() {
         super.setUp()
@@ -35,6 +39,20 @@ final class ThirdWebRPCTests: XCTestCase {
             print(blockObjects.first!)
         } catch {
             XCTFail("Expected block objects but failed: \(error).")
+        }
+    }
+    /// Testing ThirdWebRPC.getBlocksInRange
+    /// Test cases:
+    /// 1. Should throw when block range is too big
+    func testBlocksInRangeTooBig() async throws {
+        let blockCount = 1001   // Max 1000 requests per batch
+        let fromBlock = 4350000
+        let toBlock = fromBlock + blockCount - 1
+        do {
+            let blockObjects = try await rpc.getBlocksInRange(fromBlock: fromBlock, toBlock: toBlock)
+            XCTFail("Expected function to throw")
+        } catch {
+            print(error)
         }
     }
     
@@ -76,14 +94,14 @@ final class ThirdWebRPCTests: XCTestCase {
         do {
             let blockObjects = try await rpc.getBlocksInRange(fromBlock: fromBlock, toBlock: toBlock)
             print("Filtering for new contract deplyoments between in block range:\n\(fromBlock), \(toBlock)")
-            /// Filter for  new deployment TransactionObjects; whose `to` field is `nil`
+            /// Filter for  new deployment ``TransactionObject``s; whose `to` field is `nil`
             let deployTxs = blockObjects.flatMap { blockObject in
                 return blockObject.transactions.filter { tx in
                     return tx.to == nil
                 }
             }
-            print(deployTxs.count)
-            /// Map `TransactionObject`s to trasaction hashes
+            print(deployTxs.count)  // FIXME: Debugging
+            /// Map ``TransactionObject``s to trasaction hashes
             let deployTxHashes = deployTxs.map { txObj in
                 return txObj.hash
             }
@@ -92,6 +110,30 @@ final class ThirdWebRPCTests: XCTestCase {
             XCTAssertEqual(deployTxs.count, txReceiptObjects.count)
         } catch {
             XCTFail("Expected block objects but failed: \(error).")
+        }
+    }
+    
+    func testTokenName() async throws {
+        do {
+            let tokenName = try await rpc.getTokenName(contractAddress: self.allure)
+            print(tokenName)
+        } catch {
+            XCTFail("Expected token name but failed: \(error)")
+        }
+    }
+    
+    /// Testing getting the token name and symbol for a given array of contract addresses
+    /// Return object should be a dictionary with the contract addresses as keys
+    /// Test cases:
+    /// 1. Number of dictionary keys should match the number of contracts included in array
+    func testTokenInfo() async throws {
+        let contractAddresses = [allure, opepenThreadition]
+        do {
+            let tokenInfos = try await rpc.getTokenInfos(contractAddresses: contractAddresses)
+            print(tokenInfos)
+            XCTAssertEqual(tokenInfos.count, contractAddresses.count)
+        } catch {
+            XCTFail("Expected token name but failed: \(error)")
         }
     }
 
