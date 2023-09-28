bab0fb44016a146f5c84f9f09be83a395a872a06
bp094p21 <bp094p21@gmail.com>
2023-09-28 11:00:56 +1000

    init NSFretchRequests enum

---
+++ 24HZ/Core Data/NSFetchRequests.swift
@@ -0,0 +1,28 @@
+//
+// NSFetchRequests.swift
+// 24HZ
+// 
+// Created by jin on 2023-09-28
+// 
+
+
+
+import Foundation
+import CoreData
+
+enum NSFetchRequests {
+    static var enabledNewTokenListeners: NSFetchRequest<NewTokenListener> {
+        let request: NSFetchRequest<NewTokenListener> = NewTokenListener.fetchRequest()
+        request.predicate = NSPredicate(format: "isListening == %@", NSNumber(value: true))
+        return request
+    }
+    static var metadataEnabledExistingTokenListeners: NSFetchRequest<ExistingTokenListener> {
+        let request: NSFetchRequest<ExistingTokenListener> = ExistingTokenListener.fetchRequest()
+        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
+            NSPredicate(format: "listeningForMetadataEvents == %@", NSNumber(value: true)),
+            NSPredicate(format: "isListening == %@", NSNumber(value: true))
+        ])
+        request.predicate = compoundPredicate
+        return request
+    }
+}
