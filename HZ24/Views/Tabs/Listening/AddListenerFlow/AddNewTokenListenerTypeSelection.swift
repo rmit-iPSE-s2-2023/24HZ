340f62034ef1b9ffedf3f3fc16b4a6d953899f7d
bp094p21 <bp094p21@gmail.com>
2023-09-29 19:31:58 +1000

    migrate to CoreData for adding NewTokenListener

--- HZ24/Views/Tabs/Listening/AddListenerFlow/AddNewTokenListenerTypeSelection.swift
+++ HZ24/Views/Tabs/Listening/AddListenerFlow/AddNewTokenListenerTypeSelection.swift
@@ -7,15 +7,29 @@
 
 import SwiftUI
 
-// MARK: - Step 1 of AddListenerFlow
+// MARK: - Step 1 of AddListener flow for NewTokenListener
 struct AddNewTokenListenerTypeSelection: View {
     
-    /// User specified ``ERCInterfaceId``/s to listen to
-    @State private var tokenTypeSelection: [ERCInterfaceId] = []
+    /// Get a reference to viewContext
+    @Environment(\.managedObjectContext) private var viewContext
     
+    /// Fetch ``NewTokenListener``/s user has already opted into
+    @FetchRequest(entity: NewTokenListener.entity(), sortDescriptors: [])
+    private var fetchedListeners: FetchedResults<NewTokenListener>
+    
+    // MARK: States
+    /// Show alert if user selects listener type they are already subscribed to
+    @State private var showAlert = false
+    /// Keep states to track user selection
+    @State private var addCoins = false
+    @State private var addMediaWorks = false
+    /// State to use for `isActive` argument on `NavigationView`
+    @State private var goToNextScreen = false
+    
+    // MARK: Return body
     var body: some View {
         ZStack {
-            // Set black background
+            /// Background
             Color.black.edgesIgnoringSafeArea(.all)
             
             VStack {
@@ -28,35 +42,85 @@ struct AddNewTokenListenerTypeSelection: View {
                 
                 /// ``NewTokenListener``'s ``ERCInterfaceId`` options
                 /// Note: ``ERCInterfaceId``/s for ``NewtokenListener``/s that user has already opted in to should be pre-selected here
-                ForEach(ERCInterfaceId.allCases, id: \.self) { ercInterfaceId in
-                    createBlockButton(ercInterfaceId: ercInterfaceId)
+                /// "Listen for new Coins"
+                NewTokenListenerTypeOption(markdown: "New **Coins**", ercInterfaceIds: [ERCInterfaceId.erc20], toggleState: $addCoins)
+                /// "Listen for new Media Works"
+                NewTokenListenerTypeOption(markdown: "New **Media Works**", ercInterfaceIds: [ERCInterfaceId.erc721, ERCInterfaceId.erc1155], toggleState: $addMediaWorks)
+                
+                Spacer()
+                
+                /// Navigate user to next screen **AND** simulatenously perform action. Not using `.simulatenousGesture` here as user may navigate with accessibility features so will have to target multiple gestures.
+                /// Note: Using deprecated `NavigationLink` variant as `NavigationStack` is unavailable for target iOS version
+                NavigationLink("Continue", isActive: Binding<Bool>(get: { goToNextScreen }, set: { goToNextScreen = $0; print("Navigating to next screen"); createNewTokenListeners() })) {
+                    AddListenerNotificationTypeSelection()
                 }
-                
-                Spacer() // Empty space at the bottom
-                
-                // Continue Button
-                NavigationLink {
-//                    AddListenerNotificationTypeSelection()
+            }
+            // FIXME: Not sure if .alert is the right way to inform user here..
+            .alert("Already listening", isPresented: $showAlert, presenting: "hello") { details in
+                Button {
+                    //
                 } label: {
-                    customContinueButton().opacity(tokenTypeSelection.isEmpty ? 0.5 : 1)
+                    Text("OK")
                 }
-
             }
         }
     }
     
-    // Refactored block button
-    private func createBlockButton(ercInterfaceId: ERCInterfaceId) -> some View {
+    // MARK: Validations
+    private func alreadyListeningTo(ercInterfaceIds: [ERCInterfaceId]) -> Bool {
+        let fetchedInterfaceIds = fetchedListeners.map { newTokenListener in
+            return newTokenListener.ercInterfaceId!
+        }
+        return ercInterfaceIds.allSatisfy { ercInterfaceId in
+            fetchedInterfaceIds.contains(ercInterfaceId.rawValue)
+        }
+    }
+    
+    // MARK: Core Data
+    private func createNewTokenListeners() {
+        var ercInterfaceIds = [ERCInterfaceId]()
+        if addCoins {
+            ercInterfaceIds += [ERCInterfaceId.erc20]
+        }
+        if addMediaWorks {
+            ercInterfaceIds += [ERCInterfaceId.erc721, ERCInterfaceId.erc1155]
+        }
+        // Create and insert objects into context
+        for ercInterfaceId in ercInterfaceIds {
+            let newTokenListener = NewTokenListener(context: viewContext)
+            newTokenListener.id = UUID()
+            newTokenListener.isListening = true
+            newTokenListener.ercInterfaceId = ercInterfaceId.rawValue
+        }
+        print(fetchedListeners.count)
+    }
+    
+    // MARK: View components
+    private func NewTokenListenerTypeOption(markdown: String, ercInterfaceIds: [ERCInterfaceId], toggleState: Binding<Bool>) -> some View {
         Button(action: {
-            // Toggle selection state for the block
-            if tokenTypeSelection.contains(ercInterfaceId) {
-                tokenTypeSelection.removeAll { $0 == ercInterfaceId }
+            /// If user is already listening to it, gently alert user if they press the option again
+            if alreadyListeningTo(ercInterfaceIds: ercInterfaceIds) {
+                showAlert.toggle()
             } else {
-                tokenTypeSelection.append(ercInterfaceId)
+            /// Toggle the corresponding state.
+                toggleState.wrappedValue.toggle()
             }
         }) {
-            CheckableFormOption(isChecked: tokenTypeSelection.contains(ercInterfaceId))
-                .overlay(Text(ercInterfaceId.rawValue).font(.title3.bold()).foregroundColor(.black))
+            CheckableFormOption(isChecked: alreadyListeningTo(ercInterfaceIds: ercInterfaceIds) || toggleState.wrappedValue)
+                .overlay(Text(try! AttributedString(markdown: markdown)).font(.title3).foregroundColor(.black))
         }
+    }
+}
+
+// MARK: - Preview
+struct AddNewTokenListenerTypeSelection_Previews: PreviewProvider {
+    
+    static let coreDataProvider = CoreDataProvider.preview
+    
+    static var previews: some View {
+        NavigationView {
+            AddNewTokenListenerTypeSelection()
+        }
+        .environment(\.managedObjectContext, coreDataProvider.container.viewContext)
     }
 }
