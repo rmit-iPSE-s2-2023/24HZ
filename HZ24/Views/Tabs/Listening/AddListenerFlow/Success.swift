91f284294d0400cd757cff35b9eafde678e13582
bp094p21 <bp094p21@gmail.com>
2023-09-29 20:36:09 +1000

    integrate CoreData, rename AddListenerFlowView3 -> Success

---
+++ HZ24/Views/Tabs/Listening/AddListenerFlow/Success.swift
@@ -0,0 +1,61 @@
+//
+//  AddListenerFlowView3.swift
+//  a1-s3713342
+//
+//  Created by Min on 2023/08/12.
+//
+
+import SwiftUI
+
+// MARK: - Final Step of AddListenerFlow
+struct Success: View {
+    
+    let user = getDummyUser() // Create a constant user for preview
+    @State private var goToNextScreen = false
+
+    var body: some View {
+        ZStack {
+            // Set the background color to black
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                // Display a success message
+                Text("Success!")
+                    .foregroundColor(.white)
+                    .font(.title)
+                
+                // FIXME: Look into the "pop-to-root" SwiftUI pattern
+                NavigationLink("", isActive: $goToNextScreen) {
+                    ContentView()
+                        .navigationBarBackButtonHidden()
+                }
+                
+            }
+            .onAppear {
+//                 Execute the following code after a delay of 2 seconds
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    // Update the binding variables and navigate back
+                    goToNextScreen = true
+                }
+            }
+        }
+    }
+}
+
+// MARK: - Previews
+struct Success_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        /// Wrapped view to enable navigation
+        NavigationView {
+            Success()
+        }
+        .previewDisplayName("Wrapped in NavView")
+        
+        /// Unwrapped view for meaningful view debugging with: `Editor > Canvas > Show Selection`
+        Success()
+        .previewDisplayName("Unwrapped")
+        
+    }
+}
