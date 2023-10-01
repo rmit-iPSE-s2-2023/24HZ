5c17474574576322dba9fa37706ae5d5237f1f93
bp094p21 <bp094p21@gmail.com>
2023-10-01 15:04:43 +1100

    add displayTitle property to ERCInterfaceId enum

--- HZ24/Enums/ERC.swift
+++ HZ24/Enums/ERC.swift
@@ -17,6 +17,18 @@ enum ERCType: String {
 enum ERCInterfaceId: String, CaseIterable {
     case erc20 = "0x36372b07"
     case erc721 = "0x80ac58cd"
-    case erc721metadata = "0x5b5e139f"
     case erc1155 = "0xd9b67a26"
+    /// For future reference:
+    //    case erc721metadata = "0x5b5e139f"
+    
+    var displayTitle: String {
+          switch self {
+          case .erc20:
+               return "Coins"
+          case .erc721:
+               return "Media Works: 721s"
+          case .erc1155:
+               return "Media Works: 1155s"
+          }
+     }
 }
