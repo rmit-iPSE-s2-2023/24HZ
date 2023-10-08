507c02b5333be1cf7bcc32e215ae4f6ead45de2e
minchulshin1 <s3713342@student.rmit.edu.au>
2023-10-08 14:05:25 +1100

    Implement PageControl

--- HZ24/Views/Tabs/Saved/PageControl.swift
+++ HZ24/Views/Tabs/Saved/PageControl.swift
@@ -6,15 +6,55 @@
 //
 
 import SwiftUI
+import UIKit
 
-struct PageControl: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+// MARK: part b-j Integrate the UIKit and SwiftUI
+
+/// A SwiftUI wrapper around `UIPageControl` that allows page-based navigation.
+struct PageControl: UIViewRepresentable {
+    /// Total number of pages.
+    var numberOfPages: Int
+    /// Current page index (zero-based).
+    @Binding var currentPage: Int
+
+    /// Creates a coordinator to handle `UIPageControl` events.
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    /// Creates the `UIPageControl` view.
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+        // Attach an action to the page control's value changed event.
+        control.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.updateCurrentPage(sender:)),
+            for: .valueChanged)
+
+        return control
+    }
+
+    /// Updates the `UIPageControl` view with the current page.
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+
+    /// Coordinator class to handle `UIPageControl` events.
+    class Coordinator: NSObject {
+        /// Reference to the parent `PageControl` view.
+        var control: PageControl
+
+        /// Initializer for the coordinator.
+        init(_ control: PageControl) {
+            self.control = control
+        }
+
+        /// Action for updating the current page when the value of the `UIPageControl` changes.
+        @objc
+        func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
     }
 }
 
-struct PageControl_Previews: PreviewProvider {
-    static var previews: some View {
-        PageControl()
-    }
-}
