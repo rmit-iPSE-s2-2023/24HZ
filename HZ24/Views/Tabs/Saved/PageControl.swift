02c983c0e467f74c66457f1a3417b32e3135bce6
minchulshin1 <s3713342@student.rmit.edu.au>
2023-10-08 13:54:46 +1100

    Initialize SavedTab view with subviews for MVVM

---
+++ HZ24/Views/Tabs/Saved/PageControl.swift
@@ -0,0 +1,20 @@
+//
+//  PageControl.swift
+//  HZ24
+//
+//  Created by 민철 on 8/10/23.
+//
+
+import SwiftUI
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageControl_Previews: PreviewProvider {
+    static var previews: some View {
+        PageControl()
+    }
+}
---
+++ HZ24/Views/Tabs/Saved/PageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PageViewController.swift
+//  HZ24
+//
+//  Created by 민철 on 8/10/23.
+//
+
+import SwiftUI
+
+struct PageViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        PageViewController()
+    }
+}
