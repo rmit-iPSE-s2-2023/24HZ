cf84295fad0250fb76e10a3cf42b4a6a555515b4
minchulshin1 <s3713342@student.rmit.edu.au>
2023-10-11 08:50:49 +1100

    add: timestamp on captured event

--- HZ24Widget/VIew/HZ24WidgetEntryView.swift
+++ HZ24Widget/VIew/HZ24WidgetEntryView.swift
@@ -11,43 +11,35 @@ import WidgetKit
 struct HZ24WidgetsEntryView: View {
     var entry: Provider.Entry
     
-    // e
-    func eventDescription(for event: Event) -> String {
-        if event is NewTokenEvent {
-            return "new token event"
-        } else if event is MetadataEvent {
-            return "metadata event"
-        } else if event is MintCommentEvent {
-            return "mint comment event"
-        } else {
-            return "unknown event"
-        }
-    }
-    
     var body: some View {
         ZStack {
-            // Set the background to black
             Color.black.edgesIgnoringSafeArea(.all)
             HStack{
                 VStack(alignment: .leading, spacing: 3) {
                     if let event = entry.event {
-                        // Display tokenName in bold red
                         Text(event.tokenName ?? "No Recent Event")
                             .font(.headline)
                             .fontWeight(.bold)
-                            .foregroundColor(Color.red)
+                            .foregroundColor(Color.orange
+                            )
                         
-                        // Display tokenSymbol in white with a smaller font
                         Text(event.tokenSymbol ?? "")
                             .font(.subheadline)
                             .foregroundColor(Color.white)
                         
                         Spacer()
                         
-                        // Display the event type at the bottom
                         Text(eventDescription(for: event))
                             .font(.caption)
                             .foregroundColor(Color.gray)
+                        
+                        HStack {
+                            VStack(alignment: .trailing) {
+                                Text("Captured at \(eventTimestamp(for: event))")
+                                    .font(.caption2)
+                                    .foregroundColor(Color.gray)
+                            }
+                        }
                     } else {
                         Text("No Recent Event Test")
                             .foregroundColor(Color.white)
@@ -64,7 +56,19 @@ struct HZ24Widgets_Previews: PreviewProvider {
     static var previews: some View {
         let context = PersistenceController.preview.container.viewContext
         let event = try? Provider().getLatestEvent(from: context)
-        return HZ24WidgetsEntryView(entry: SimpleEntry(date: Date(), event: event))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+
+        Group {
+            HZ24WidgetsEntryView(entry: SimpleEntry(date: Date(), event: event))
+                .previewContext(WidgetPreviewContext(family: .systemSmall))
+                .previewDisplayName("Small Widget")
+
+            HZ24WidgetsEntryView(entry: SimpleEntry(date: Date(), event: event))
+                .previewContext(WidgetPreviewContext(family: .systemMedium))
+                .previewDisplayName("Medium Widget")
+
+            HZ24WidgetsEntryView(entry: SimpleEntry(date: Date(), event: event))
+                .previewContext(WidgetPreviewContext(family: .systemLarge))
+                .previewDisplayName("Large Widget")
+        }
     }
 }
