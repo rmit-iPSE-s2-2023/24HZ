0671fb4163c8903a32bbf8dff61e89a6d10b19c3
minchulshin1 <s3713342@student.rmit.edu.au>
2023-10-11 06:33:49 +1100

    add: HZ24 widget extension

---
+++ HZ24Widget/HZ24Widget.swift
@@ -0,0 +1,29 @@
+//
+//  HZ24Widget.swift
+//  HZ24Widget
+//
+//  Created by Min on 09/10/23.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+import CoreData
+
+
+@main
+struct HZ24Widgets: Widget {
+    let kind: String = "MyTextWidgets"
+    
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            HZ24WidgetsEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This widget shows the latest event.")
+    }
+}
+
+
+
+
