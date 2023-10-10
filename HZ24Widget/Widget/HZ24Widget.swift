d7c8a2b5de2c291dcdef85d89349b14873955f2a
minchulshin1 <s3713342@student.rmit.edu.au>
2023-10-11 07:12:49 +1100

    update: widget configuration

--- HZ24Widget/Widget/HZ24Widget.swift
+++ HZ24Widget/Widget/HZ24Widget.swift
@@ -14,15 +14,15 @@ import CoreData
 @main
 struct HZ24Widgets: Widget {
     let persistenceController = PersistenceController.shared
-    let kind: String = "MyTextWidgets"
+    let kind: String = "24HZ"
     
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             HZ24WidgetsEntryView(entry: entry)
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
-        .configurationDisplayName("My Widget")
-        .description("This widget shows the latest event.")
+        .configurationDisplayName("Latest Event")
+        .description("Quickly view the latest event right from your home screen.")
     }
 }
 
