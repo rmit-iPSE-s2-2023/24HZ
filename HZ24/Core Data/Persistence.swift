66c9aa940f4814f28fa3ff1f8636ef458988bd2b
minchulshin1 <s3713342@student.rmit.edu.au>
2023-10-12 04:25:27 +1100

    remove: test data from PersistenceController preview initialization

--- HZ24/Core Data/Persistence.swift
+++ HZ24/Core Data/Persistence.swift
@@ -4,29 +4,19 @@
 //
 //  Created by 민철 on 11/10/23.
 //
+
 // MARK: JUST made this for widget
 import CoreData
 
 struct PersistenceController {
     static let shared = PersistenceController ()
     static var preview: PersistenceController = {
-        let result = PersistenceController (inMemory: true) // true
+        let result = PersistenceController (inMemory: true) //here
         let viewContext = result.container.viewContext
+        
         // populate this with some preview data if required.
-        
-        // TEST
-        let newTokenEvent = NewTokenEvent(context: viewContext)
-        newTokenEvent.blockHash = "0x"
-        newTokenEvent.blockNumber = Int64(1)
-        newTokenEvent.contractAddress = "0xabc"
-        newTokenEvent.id = UUID()
-        newTokenEvent.saved = false
-        newTokenEvent.timestamp = Date()
-        newTokenEvent.tokenName = "Preview Token1"
-        newTokenEvent.tokenSymbol = "PRE"
-        newTokenEvent.transactionHash = "0x"
-        newTokenEvent.deployerAddress = "0xdeployer"
-        
+
+
         do {
             try viewContext.save()
         } catch {
@@ -35,26 +25,26 @@ struct PersistenceController {
         }
         return result
     }()
-    
+
     let container: NSPersistentContainer
-    
+
     init (inMemory: Bool = false) {
-        
+
         container = NSPersistentContainer (name: "Model")
-        
+
         let url = URL.storeURL(for: "group.com.Min-Jin.24HZ", databaseName: "HZ24")
-        let storeDescription = NSPersistentStoreDescription (url: url)
+        let storeDescription = NSPersistentStoreDescription(url: url)
         container.persistentStoreDescriptions = [storeDescription]
-        
+
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
-        container.loadPersistentStores (completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                fatalError( "Unresolved error \(error), \(error.userInfo)")
+                fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
-        container.viewContext.automaticallyMergesChangesFromParent=true
+        container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
 
@@ -64,8 +54,7 @@ public extension URL {
         guard let fileContainer = FileManager.default.containerURL (forSecurityApplicationGroupIdentifier: appGroup) else {
             fatalError ("Unable to create URL for \(appGroup)")
         }
-        return fileContainer.appendingPathComponent("\(databaseName),sqlite")
+        return fileContainer.appendingPathComponent("\(databaseName).sqlite")
     }
 }
-
 
