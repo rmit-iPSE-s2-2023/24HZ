489268a59cae3096cfff45088d3570895ce450d5
bp094p21 <bp094p21@gmail.com>
2023-10-11 19:38:53 +1100

    add: additional preferences for ExistingTokenListener

--- HZ24/Views/Tabs/Listening/ListenerSettings.swift
+++ HZ24/Views/Tabs/Listening/ListenerSettings.swift
@@ -15,7 +15,12 @@ struct ListenerSettings: View {
     var listener: Listener
     
     @State private var deleteAlert = false  // To control the alert visibility
+    
+    ///
     @State private var isListening: Bool
+    /// Preferences for ``ExistingTokenListener``
+    @State private var listenForMetadataEvents: Bool
+    @State private var listenForMintCommentEvents: Bool
     
     @State private var eventsOnFeed: Bool = true
     @State private var dailyEmail: Bool = false
@@ -25,98 +30,153 @@ struct ListenerSettings: View {
     init(listener: Listener) {
         self.listener = listener
         _isListening = State(initialValue: listener.isListening)
+        if let listener = listener as? ExistingTokenListener {
+            _listenForMetadataEvents = State(initialValue: listener.listeningForMetadataEvents)
+            _listenForMintCommentEvents = State(initialValue: listener.listeningForMintCommentEvents)
+        } else {
+            _listenForMetadataEvents = State(initialValue: false)
+            _listenForMintCommentEvents = State(initialValue: false)
+        }
     }
     
     var body: some View {
-        ZStack {
-            // Background color
-            Color.black.edgesIgnoringSafeArea(.all)
+        
+        VStack {
             
-            VStack() {
-                
-                /// Title of ``Listener``
+            /// Title of ``Listener``
+            HStack {
+                Text(listener.displayTitle ?? "Unknown")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .padding(.leading)
+                Spacer()
+            }
+            .padding(.bottom, 8)
+            
+            if let listener = listener as? ExistingTokenListener {
                 HStack {
-                    Text(listener.displayTitle ?? "Unknown")
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
+                    Text("\(listener.contractAddress ?? "")")
+                        .font(.subheadline)
                         .padding(.leading)
+                        .foregroundColor(.secondary)
                     Spacer()
                 }
-                
-                List {
-                    
-                    /// Setting to enable/mute the ``Listener``
-                    Section {
-                        NavigationLink(destination: ListenerEnable(listener: listener)) {
-                            HStack {
-                                Text("Listener")
-                                Spacer()
-                                Text(listener.isListening ? "On" : "Off")
-                                    .foregroundStyle(.gray)
+            }
+            
+            List {
+                /// Setting to enable/mute the ``Listener``
+                Section {
+                    NavigationLink(destination: ListenerEnable(listener: listener)) {
+                        HStack {
+                            Text("Listener")
+                            Spacer()
+                            Text(listener.isListening ? "On" : "Off")
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+//                List {
+//                    Toggle(isOn: $isListening) {
+//                        Text("Listener")
+//                    }
+//                    .onChange(of: isListening) { newValue in
+//                        /// Update MO
+//                        listener.isListening = newValue
+//                        do {
+//                            try viewContext.save()
+//                        } catch {
+//                            print(error)
+//                        }
+//                    }
+//                }
+//                .navigationTitle("Listener")
+//                .navigationBarTitleDisplayMode(.inline)
+                /// ABI Event Type Preferences
+                if let listener = listener as? ExistingTokenListener {
+                    Section("Event Types") {
+                        Toggle(isOn: $listenForMetadataEvents) {
+                            Text("Metadata updates")
+                        }
+                        .onChange(of: listenForMetadataEvents) { newValue in
+                            listener.listeningForMetadataEvents = newValue
+                            do {
+                                try viewContext.save()
+                            } catch {
+                                print(error)
+                            }
+                        }
+                        Toggle(isOn: $listenForMintCommentEvents) {
+                            Text("Mints with comments")
+                        }
+                        .onChange(of: listenForMintCommentEvents) { newValue in
+                            listener.listeningForMintCommentEvents = newValue
+                            do {
+                                try viewContext.save()
+                            } catch {
+                                print(error)
                             }
                         }
                     }
-                    
-                    /// Notificaiton Settings
-                    Section("Notifications") {
-                        Toggle(isOn: $eventsOnFeed) {
-                            Text("Feed")
-                        }
-                        Toggle(isOn: $dailyEmail) {
-                            Text("Daily email")
-                        }
-                        Toggle(isOn: $everyEventEmail) {
-                            Text("Email for every event")
-                        }
-                        Toggle(isOn: $pushNotifications) {
-                            Text("Push")
-                        }
-                        
-                    }
-                    .disabled(true)
                 }
-                .listStyle(.insetGrouped)
+
                 
-                Spacer()
-                
-                /// Delete button to remove ``Listener`` from store
-                //                DeleteListenerButton
-                Button {
-                    /// Show alert to delete ``Listener``
+                /// Notificaiton Settings
+                Section("Notifications") {
+                    Toggle(isOn: $eventsOnFeed) {
+                        Text("Feed")
+                    }
+                    Toggle(isOn: $dailyEmail) {
+                        Text("Daily email")
+                    }
+                    Toggle(isOn: $everyEventEmail) {
+                        Text("Email for every event")
+                    }
+                    Toggle(isOn: $pushNotifications) {
+                        Text("Push")
+                    }
+                    
+                }
+                .disabled(true)
+            }
+            .listStyle(.insetGrouped)
+            
+            Spacer()
+
+            /// Delete button to remove ``Listener`` from store
+            // DeleteListenerButton
+            Button {
+                /// Show alert to delete ``Listener``
+                deleteAlert.toggle()
+            } label: {
+                Text("Delete this listener")
+                    .font(.title3)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            .alert("Delete listener?", isPresented: $deleteAlert) {
+                // Delete
+                Button("Delete", role: .destructive) {
+                    /// Delete ``Listener``
+                    viewContext.delete(listener)
+                    do {
+                        try viewContext.save()
+                    } catch {
+                        print(error)
+                    }
+                    
+                }
+                Button("Cancel", role: .cancel) {
                     deleteAlert.toggle()
-                } label: {
-                    Text("Delete this listener")
-                        .font(.title3)
-                        .foregroundColor(.red)
-                        .padding()
-                }
-                .alert("Delete listener?", isPresented: $deleteAlert) {
-                    // Delete
-                    Button("Delete", role: .destructive) {
-                        /// Delete ``Listener``
-                        viewContext.delete(listener)
-                        do {
-                            try viewContext.save()
-                        } catch {
-                            print(error)
-                        }
-                        
-                    }
-                    Button("Cancel", role: .cancel) {
-                        deleteAlert.toggle()
-                    }
-                    
-                    
-                    // Cancel
-                } message: {
-                    Text("Would you like to stop listening to this listener?")
                 }
                 
                 
+            // Cancel
+            } message: {
+                Text("Would you like to stop listening to this listener?")
             }
         }
-        .preferredColorScheme(.dark)    // TODO: Make light/dark color variants. Defaulting everything to dark mode for now
+        .navigationTitle("Listener settings")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
@@ -131,6 +191,7 @@ struct ListenerSettings_Previews: PreviewProvider {
         listener.isListening = true
         return listener
     }
+    static let existingTokenListener = PreviewModels.enjoyEthereumListener
     
     static var previews: some View {
         NavigationView {
@@ -138,8 +199,18 @@ struct ListenerSettings_Previews: PreviewProvider {
                 .environment(\.managedObjectContext, coreDataProvider.container.viewContext)
         }
         
+        NavigationView {
+            ListenerSettings(listener: existingTokenListener)
+                .environment(\.managedObjectContext, coreDataProvider.container.viewContext)
+        }
+        
         ListenerSettings(listener: listener)
             .environment(\.managedObjectContext, coreDataProvider.container.viewContext)
+        
+        ListenerSettings(listener: existingTokenListener)
+            .environment(\.managedObjectContext, coreDataProvider.container.viewContext)
+        
+        
     }
 }
 
