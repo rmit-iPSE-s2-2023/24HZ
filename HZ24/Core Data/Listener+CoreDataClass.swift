79f615fe7b4d687bf41367e441ac62afd882c48c
bp094p21 <bp094p21@gmail.com>
2023-10-14 21:23:57 +1100

    doc: update DocC comments

--- HZ24/Core Data/Listener+CoreDataClass.swift
+++ HZ24/Core Data/Listener+CoreDataClass.swift
@@ -13,7 +13,7 @@ import CoreData
 
 /// An object that listens to a specific set of events.
 ///
-/// This is a Core Data parent entity that represents a _listener_ that the user creates. The ``CoreDataProvider`` fetches all the ``Listener``(s) that the user has enabled, and uses the additional attributes of their child entities (``NewTokenListener`` and ``ExistingTokenListener``) to capture the corresponding set of ``Event``(s).
+/// This is a Core Data parent entity that represents a _listener_ that the user creates. The Core Data stack fetches all the ``Listener``s that the user has enabled, and uses the additional attributes of their child entities (``NewTokenListener`` and ``ExistingTokenListener``) to capture the relevant ``Event``s from the network..
 @objc(Listener)
 public class Listener: NSManagedObject {
 
